78bcb57b412c02753698e96095a3784a5ef26010
dyglo <dyglo@github.com>
2024-12-09 14:05:20 -0800

    refactor: remove all prisma and database dependencies

--- src/app/api/auth/auth.config.ts
+++ src/app/api/auth/auth.config.ts
@@ -1,21 +1,13 @@
-import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/prisma";
 import { AuthOptions } from "next-auth";
-import type { Adapter } from "next-auth/adapters";
-
-const customPrismaAdapter = PrismaAdapter(prisma) as Adapter;
 
 export const authOptions: AuthOptions = {
-  adapter: customPrismaAdapter,
   providers: [
     // Add your providers here
   ],
   callbacks: {
     async session({ session, user }) {
       if (session?.user) {
-        session.user.role = user.role || '';
-        // Do not assign id here as it's not in the defined type
+        session.user.role = 'user';
       }
       return session;
     },
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,19 +1,13 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from './prisma';
 import { AuthOptions } from "next-auth";
-import type { Adapter } from "next-auth/adapters";
-
-const customPrismaAdapter = PrismaAdapter(prisma) as Adapter;
 
 export const authOptions: AuthOptions = {
-  adapter: customPrismaAdapter,
   providers: [
     // Add your providers here
   ],
   callbacks: {
     async session({ session, user }) {
       if (session?.user) {
-        session.user.role = user.role || '';
+        session.user.role = 'user';
       }
       return session;
     },
